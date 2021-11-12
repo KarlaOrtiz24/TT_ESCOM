476a0a3199f8cc311892934b0fc7b9dc6bbfa3d1
Angel Piñon <85593752+arpinon@users.noreply.github.com>
2021-11-11 23:48:45 -0600

    Update ExtraccionFondo.py
    Se añade la función que  realiza la operación and de la imagen y su mascara

--- ExtraccionFondo.py
+++ ExtraccionFondo.py
@@ -1,17 +1,17 @@
 from PIL import Image
 
-imagen = Image.open("B31.png")
+imagen = Image.open("B.png")
 imagen = imagen.convert('RGB')
 maxsize = (200, 200)
 imagen.thumbnail(maxsize)
 
 def Segmentar(imagen):
     #Fondo blanco
-    #lower = ([50,10,0])
-    #high = ([235,180,170])
+    lower = ([50,10,0])
+    high = ([235,180,170])
     #Fondo verde
-    lower = ([120,100,56])
-    high = ([255,180,150])
+    #lower = ([120,100,56])
+    #high = ([255,180,150])
     imagenResultado = Image.new('1', imagen.size)
     ancho,alto = imagen.size
     for i in range (ancho):
@@ -23,5 +23,20 @@ def Segmentar(imagen):
                 imagenResultado.putpixel((i,j),1)
     return imagenResultado
 
+def Andimagenes(imagen, mascara):
+    imagenResultado = Image.new('RGB', imagen.size)
+    ancho,alto = imagen.size
+    for i in range (ancho):
+        for j in range (alto):
+            pixelm = mascara.getpixel((i,j))
+            pixeli = imagen.getpixel((i,j))
+            if (pixelm == 1):
+                imagenResultado.putpixel((i,j),pixeli)
+            else:
+                imagenResultado.putpixel((i,j),(0,0,0))
+    return imagenResultado
+
 mascara = Segmentar(imagen)
 mascara.show()
+R = Andimagenes(imagen, mascara)
+R.show()
