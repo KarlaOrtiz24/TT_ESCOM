7f53918442f6da7b45c421c9c9a67f759d146cab
Angel Piñon <85593752+arpinon@users.noreply.github.com>
2021-11-11 21:07:17 -0600

    Nuevo archivo: Extracción de Fondo
    Programa que extrae los fondos de las imágenes estáticas

---
+++ ExtraccionFondo.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+imagen = Image.open("B31.png")
+imagen = imagen.convert('RGB')
+maxsize = (200, 200)
+imagen.thumbnail(maxsize)
+
+def Segmentar(imagen):
+    #Fondo blanco
+    #lower = ([50,10,0])
+    #high = ([235,180,170])
+    #Fondo verde
+    lower = ([120,100,56])
+    high = ([255,180,150])
+    imagenResultado = Image.new('1', imagen.size)
+    ancho,alto = imagen.size
+    for i in range (ancho):
+        for j in range (alto):
+            r,g,b = imagen.getpixel((i,j))
+            if (r<lower[0] or r>high[0] and g<lower[1] or g>high[1] and b<lower[2] or b>high[2]):
+                imagenResultado.putpixel((i,j),0)
+            else:
+                imagenResultado.putpixel((i,j),1)
+    return imagenResultado
+
+mascara = Segmentar(imagen)
+mascara.show()
