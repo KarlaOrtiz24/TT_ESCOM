0cf8852c20b7836262333810b0ac6c8b5ff84303
YoarihtM <yoariht.mc37@gmail.com>
2022-03-09 18:04:28 -0600

    Mejora en la lectura de los archivos y enrutamiento de las carpetas

--- Pruebas/Python/Routes/Routes.py
+++ Pruebas/Python/Routes/Routes.py
@@ -1,8 +1,25 @@
 import os
 
-def getRutaActual(file):
-    return os.path.dirname(file)
+def getRutaActual(archivoActual):
+    return os.path.dirname(archivoActual)
 
-def getRutaPadre(file):
-    return os.path.join(getRutaActual(file), '..')
+def getRutaPadre(archivoActual):
+    return os.path.join(getRutaActual(archivoActual), '..')
 
+def juntarConPadre(archivoActual, rutaInteres):
+    return os.path.join(getRutaPadre(archivoActual), rutaInteres)
+
+def getCarpetasRuta(ruta):
+    carpetas = []
+    
+    with os.scandir(ruta) as directorios:
+        for directorio in directorios:
+            carpetas.append(str(directorio).split("'")[1])
+            
+        return carpetas
+    
+def juntarRutas(ruta1, ruta2):
+    return os.path.join(ruta1, ruta2)
+
+def getArchivosCarpeta(ruta):
+    return os.listdir(ruta)
