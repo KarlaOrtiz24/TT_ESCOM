b644451456e640f5264c5e355ea0429a09cf41f6
YoarihtM <yoariht.mc37@gmail.com>
2022-03-27 21:31:41 -0600

    pruebas creacion de json

--- Pruebas/Python/DenseTrajectoriesPrueba.py
+++ Pruebas/Python/DenseTrajectoriesPrueba.py
@@ -22,8 +22,7 @@ bgr4json = {}
 rgb4json = {}
 
 num_frames = 0
-fil = 0
-col = 0
+pixel = 0
 
 lista = []
 
@@ -34,18 +33,22 @@ for frame in bgr.values():
     bgr4json.setdefault(nombre_frame, {})
     
     for fila in range(len(frame)):
-        nombre_fila = 'fila_' + str(fila)
-        
-        bgr4json[nombre_frame].setdefault(nombre_fila, {})
-
         for columna in range(len(frame[fila])):
-            nombre_col = 'columna_' + str(columna)
-            # print(nombre_frame, nombre_fila, nombre_col)
+            
+            if pixel == (len(frame) * len(frame[fila])) - 1:
+                pixel = 0
+            else:
+                pixel += 1
+            
+            nombre_pixel = 'pixel_' + str(pixel)
+
+            # print(nombre_frame, nombre_pixel)
+
             
             bgr_valores = dict(zip('bgr', frame[fila][columna].tolist()))
             # print(bgr_valores)
             
-            bgr4json[nombre_frame][nombre_fila].setdefault(nombre_col, bgr_valores)
+            bgr4json[nombre_frame].setdefault(nombre_pixel, bgr_valores)
 
 
 # print(bgr4json)
