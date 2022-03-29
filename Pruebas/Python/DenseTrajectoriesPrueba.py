2a79fd3e2ba9a71d4bbccce65e55fc692965fe40
YoarihtM <yoariht.mc37@gmail.com>
2022-03-29 16:10:58 -0600

    Se realizaron los cambios para que escriba los datos de los pixeles de cada frame en un archivo csv aunque es muy tardado y aun no se si la memoria se estanque aunque parece que no

--- Pruebas/Python/DenseTrajectoriesPrueba.py
+++ Pruebas/Python/DenseTrajectoriesPrueba.py
@@ -1,9 +1,9 @@
 from Utilities import Video 
 import Routes.Routes as routes
-import json
+import csv
 
 ruta_destino = routes.juntarConPadre(__file__, 'VideosPrueba')
-ruta_json = routes.juntarConPadre(__file__, 'JSON')
+ruta_csv = routes.juntarConPadre(__file__, 'CSV')
 
 ruta_origen = routes.juntarConPadre(__file__, '..')
 ruta_origen = routes.juntarRutas(ruta_origen, 'Aprendizaje_Dinamico')
@@ -15,6 +15,48 @@ for archivo in archivos_ruta_origen:
     
     if extension[1] == 'mp4':
         ruta_video = routes.juntarRutas(ruta_origen, archivo)
+        ruta_dataframe_bgr = routes.juntarRutas(ruta_csv, extension[0] + '_bgr.csv')
+        ruta_dataframe_rgb = routes.juntarRutas(ruta_csv, extension[0] + '_rgb.csv')
         bgr, rgb = Video.dtVideoJson(ruta_video, ruta_destino, extension[0])
+        
+        pixeles = []
+        pixel = []
+        
+        info_video = []
+        
+        frames = 0
+        
+        for frame in bgr.values():
+            pixeles.clear()
+            nombre_frame = 'frame_' + str(frames)
+            
+            for fila in range(len(frame)):
+                nombre_fila = 'fila_' + str(fila)
+                
+                for columna in range(len(frame[fila])):
+                    
+                    nombre_columna = 'columna_' + str(columna)
+                    
+                    pixel.clear()
+                    pixel.append(nombre_frame)
+                    pixel.append(nombre_fila)
+                    pixel.append(nombre_columna)
+                    
+                    for dato in range(len(frame[fila][columna])):
+                        pixel.append(int(frame[fila][columna][dato]))
+                    
+                    # print(pixel)
+                    pixeles.append(pixel)
+                    
+            frames += 1
+        
+        # print(pixeles)
+        
+        with open(ruta_dataframe_bgr, 'w', newline='') as File:
+            writer = csv.writer(File)
+            writer.writerows(pixeles)
+        
+        print('ESCRIUTA DE ' + extension[0] + '_bgr.csv COMPLETADA' )
+        
     else:
         continue
