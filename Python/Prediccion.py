0fe5d0f24a5c7bd1cf203486914e5be1e2afbf85
Karla <karlao24c@gmail.com>
2022-03-30 19:23:36 -0600

    Pruebas

--- Python/Prediccion.py
+++ Python/Prediccion.py
@@ -17,7 +17,7 @@ from spacy import displacy
 #print(labels)
 # Lectura de la camara
 
-modelo = 'C:/Users/Karla/TT_ESCOM/Convolucional.h5'
+modelo = 'C:/Users/Karla/TT_ESCOM/ABECEDARIO.h5'
 cnn = load_model(modelo)
 
 direccion = 'C:/Users/Karla/TT_ESCOM/Aprendizaje_Abecedario'
@@ -37,52 +37,7 @@ manos = clase_manos.Hands()
 # Dibujo de las manos
 dibujo = mp.solutions.drawing_utils
 
-""""
-def obtener_letra_espacio(cont_letra, letra, numero):
-    if numero == 1:
-        if cont_letra == 1:
-            return letra.upper()
-        else:
-            return letra
-    elif numero == 2:
-        return letra + " "
-    else:
-        print("Escribio un numero o valor incorrecto")
 
-
-def obtener_frase():
-    print("Desea guardar la letra: 1 Si - 0 No")
-    valor = int(input())
-    if valor == 1:
-        print("Desea agregar un espacio: 1 Si - 0 No")
-        valor2 = int(input())
-        if valor2 == 1:
-            return 1
-        elif valor2 == 0:
-            return 2
-        else:
-            print("Valor erroneo")
-    else:
-        print("\n")
-
-
-def tokenizar(frase):
-    tokens = frase.split()
-    print(tokens)
-    return tokens
-
-
-def analisis_sintáctico(frase):
-    nlp = spacy.load("es_core_news_sm")
-    doc = nlp(frase)
-    return [token.text for token in doc]
-
-
-def analisis_semantico(frase):
-    nlp = spacy.load("es_core_news_sm")
-    doc = nlp(frase)
-    return displacy.render(doc, style="ent", jupyter="TRUE")
-"""
 
 while (1):
     cont_letras = 0
