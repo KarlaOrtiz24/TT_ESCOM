e28c1bf85ebcdcb61f7731ea19d6b0e17ad09af3
Karla <karlao24c@gmail.com>
2022-03-31 17:08:31 -0600

    Cambios prediccion

--- Python/deteccionManos.py
+++ Python/deteccionManos.py
@@ -19,60 +19,16 @@ from spacy import displacy
 # load saved model from PC
 model = load_model(r'C:\Users\Karla\TT_ESCOM\ABECEDARIO.h5')
 model.summary()
-data_dir = 'Aprendizaje_Abecedario'
+data_dir = r'C:\Users\Karla\TT_ESCOM\Aprendizaje_Abecedario'
 #getting the labels form data directory
-labels = sorted(os.listdir(data_dir))
+labels = (os.listdir(data_dir))
 print('labels', labels)
-#labels[-1] = 'Nothing'
+#labels[1] = 'Nothing'
 #print(labels)
 
 #initiating the video source, 0 for internal camera
 cap = cv2.VideoCapture(0)
 
-def obtener_letra_espacio(cont_letra, letra, numero):
-    if numero == 1:
-        if cont_letra == 1:
-            return letra.upper()
-        else:
-            return letra
-    elif numero == 2:
-        return letra + " "
-    else:
-        print("Escribio un numero o valor incorrecto")
-
-
-def obtener_frase():
-    print("Desea guardar la letra: 1 Si - 0 No")
-    valor = int(input())
-    if valor == 1:
-        print("Desea agregar un espacio: 1 Si - 0 No")
-        valor2 = int(input())
-        if valor2 == 1:
-            return 1
-        elif valor2 == 0:
-            return 2
-        else:
-            print("Valor erroneo")
-    else:
-        print("\n")
-
-
-def tokenizar(frase):
-    tokens = frase.split()
-    print(tokens)
-    return tokens
-
-
-def analisis_sintáctico(frase):
-    nlp = spacy.load("es_core_news_sm")
-    doc = nlp(frase)
-    return [token.text for token in doc]
-
-
-def analisis_semantico(frase):
-    nlp = spacy.load("es_core_news_sm")
-    doc = nlp(frase)
-    return displacy.render(doc, style="ent", jupyter="TRUE")
 
 while(True):
     
