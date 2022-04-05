e3cc690327f36ff274b644cda1ab0d6fa44e21d9
Karla <karlao24c@gmail.com>
2022-04-04 21:17:19 -0500

    Correccion al directorio

--- Python/ActionDetection.py
+++ Python/ActionDetection.py
@@ -116,10 +116,9 @@ sequence_length = 30
 # Folder start
 start_folder = 30
 for action in actions: 
-    dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
-    for sequence in range(1,no_sequences+1):
+    for sequence in range(no_sequences):
         try: 
-            os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
         except:
             pass
 
