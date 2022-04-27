d5b00cb6548e7b80ba7a0093070e2a58586b55a1
Karla <karlao24c@gmail.com>
2022-04-26 20:49:55 -0500

    Red neuronal r

--- Python/ActionDetection.py
+++ Python/ActionDetection.py
@@ -13,6 +13,9 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.callbacks import TensorBoard
+from sklearn.metrics import confusion_matrix, classification_report
+import pandas as pd 
+import seaborn as sn 
 
 mp_holistic = mp.solutions.holistic # Modelo Holistico
 mp_drawing = mp.solutions.drawing_utils # Dibuja puntos
@@ -216,131 +219,29 @@ for action in actions:
 np.array(sequences).shape
 np.array(labels).shape
 X = np.array(sequences)
-X.shape
+print(X.shape)
 y = to_categorical(labels).astype(int)
 y
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 y_test.shape
-'''
- ## Nos situamos en la dirección actual 
-actual_path = os.path.join(os.path.dirname(__file__), '..')
-
-list_directorios = [] ##Guarda las lista de directorios
-    ##Checa que lo que este, sean carpetas, si son carpetas lo añade a la lista. 
-with os.scandir(actual_path) as directories:
-    for directory in directories:
-        if directory.is_dir():
-            list_directorios.append(directory)
-
-##image_dir es la ruta de las carpetas de las imagenes. 
-image_dir = os.path.join(actual_path, list_directorios[4])
-print(image_dir)
-
-##print("IMAGE DIR", image_dir)
-list_img_dir = [] ##Son las carpetas
-with os.scandir(image_dir) as img_directories:
-    for img_dir in img_directories:
-        list_img_dir.append(img_dir)
-
-list_img_real_directorio = [] ##Lista de directorios de las imagenes 
-clases = []
-for img_dir in list_img_dir:
-    clases.append(img_dir.name)
-    list_img_real_directorio.append(os.path.join(image_dir, img_dir))
-##print("LIST", clases)
-files_cant = 0
-
-labels = [] #Lista de etiquetas
-dircount = []
-images =[] #Lista de imagenes
-
-for img_dir in list_img_real_directorio:
-    #  print('Directorio:', img_dir)
-    files = os.listdir(img_dir)
-    dircount.append(len(files))
-
-    for file in files:
-        ##print('Archivo leido:', file)
-        img = cv2.imread(os.path.join(img_dir,file))
-        images.append(img)
-        img_array = np.asarray(img)
-        ##print("IMG ARRAY", img_array)
-        #print(len(img_array))
-        #print("IMG", img)
-        files_cant += 1
-
-##Creacion de etiquetas, etiquetado de todos los datos. 
-indice = 0 
-for cant in dircount: 
-    for i in range(cant): 
-        labels.append(indice)
-    indice+=1
-print("Etiquetas: ",len(labels))
-
-    ##Clases, asignacion de las clases con un índice, EJEMPLO CLASE A índice 0. 
-Abecedario_fijo=[]
-indice=0
-for directorio in list_img_real_directorio:
-    name = directorio.split(os.sep)
-    print(indice , name[len(name)-1])
-    Abecedario_fijo.append(name[len(name)-1])
-    indice=indice+1
-
-y = np.array(labels)
-X = np.array(images) #Se convierten las imagenes a datos numpy 
-
-    ##print("X", X)
-classes = np.unique(y)
-nClasses = len(classes)
-print('Total de clases : ', nClasses) #imprime el total de las clases, clases 21
-print('Lista de clases: ', classes)  #Nos dice las clases
 
 
 
-
-train_X,test_X,train_Y,test_Y = train_test_split(X,y,test_size=0.2)
-print('Aprendizaje:', train_X.shape, train_Y.shape)#80% aprendizaje
-print('Recuperación:', test_X.shape, test_Y.shape)#20% recuperación
-
-train_X = train_X.astype('float32')
-test_X = test_X.astype('float32')
-train_X = train_X/255
-test_X = test_X/255 #Normalizarlo, 0, 1 
-
-train_Y_one_hot = to_categorical(train_Y)
-test_Y_one_hot  = to_categorical(test_Y)
-
-print('Etiqueta original: ', train_Y[0])
-print('Despues de la conversion: ', train_Y_one_hot[0]) #A(1, 0,0,0,0,0,0,0,0,0)
-
-train_X, valid_X, train_label, valid_label = train_test_split(train_X, train_Y_one_hot, test_size = 0.2, random_state = 4)
-
-    # el método shape da la cantidad de datos que contiene un arreglo
-
-    # print('train_X: ', train_X.shape)
-    # print('valid_X: ', valid_X.shape)
-
-    # print('train_label:', train_label.shape)
-    # print('valid_label:', valid_label.shape)
-
-
-print(train_X.shape,valid_X.shape,train_label.shape,valid_label.shape)
-'''
-
-
-
+#Red neuronal 
+#El modelo es secuencial, 3 capas LSTM  y 3 capas densas con distintas neuronas en cada una. 
 log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
 model = Sequential()
-model.add(LSTM(100, return_sequences=True, activation='relu', input_shape=(20,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu', input_shape=(20,1662)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
 model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(1000, activation='relu'))
 model.add(Dense(800, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=100, callbacks=[tb_callback])
+model.fit(X_train, y_train, epochs=80, callbacks=[tb_callback])
 model.summary()
+
 
 
 test_eval = model.evaluate(X_test, y_test, verbose=1)
