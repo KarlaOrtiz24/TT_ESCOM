4b2c9537bb08b963daa2194ef1d9fafe46463551
YoarihtM <yoariht.mc37@gmail.com>
2022-05-29 14:01:09 -0500

    cambios de splash screen

--- GUI/src/nlp.py
+++ GUI/src/nlp.py
@@ -10,21 +10,23 @@ def nlp(Frase):
     dicts = doc.to_dict()
 
     lemma = []
-    for token in dicts[0]:
-        lemma.append(token['lemma'])
+    try:
+        for token in dicts[0]:
+            lemma.append(token['lemma'])
 
-        #Comprobamos que el token no sea un articulo
-        if(token['upos'] == "DET" and 'PronType=Art' in token['feats']):
-            lemma.pop()
+            #Comprobamos que el token no sea un articulo
+            if(token['upos'] == "DET" and 'PronType=Art' in token['feats']):
+                lemma.pop()
 
-        #Comprobamos que no aparezcan los verbos ser o estar
-        if(token['lemma'] == "estar" or token['lemma'] == "ser"):
-            lemma.pop()
+            #Comprobamos que no aparezcan los verbos ser o estar
+            if(token['lemma'] == "estar" or token['lemma'] == "ser"):
+                lemma.pop()
 
-        #Comprobamos la cantidad de los sustantivos
-        if (token['upos'] == "NOUN" and'Number=Plur' in token['feats']):
-            lemma.append('muchos')
-
+            #Comprobamos la cantidad de los sustantivos
+            if (token['upos'] == "NOUN" and'Number=Plur' in token['feats']):
+                lemma.append('muchos')
+    except:
+        lemma = "No fue posible realizar la traducción"
 
     return lemma
 
